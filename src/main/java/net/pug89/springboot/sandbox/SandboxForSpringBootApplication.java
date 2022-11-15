244683ee726cf0918c7e27c6a4530351d92aa007
Masatoshi Namiki <m-namiki@bns-g.com>
2022-11-15 10:25:55 +0900

    add springboot

---
+++ src/main/java/net/pug89/springboot/sandbox/SandboxForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package net.pug89.springboot.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxForSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/pug89/springboot/sandbox/SandboxForSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.pug89.springboot.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxForSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
